02c4df9b426d0940cf9723c2d502f2e06af358cf
gapiyka <50524296+gapiyka@users.noreply.github.com>
2022-06-11 15:05:41 +0300

    feat: add commands
    >`print` command
    >`cat` command

---
+++ engine/command.go
@@ -0,0 +1,23 @@
+package engine
+
+import (
+	"fmt"
+)
+
+type PrintCommand struct {
+	Arg string
+}
+
+func (print *PrintCommand) Execute(loop Handler) {
+	fmt.Println(print.Arg)
+}
+
+type CatCommand struct {
+	Arg1 string
+	Arg2 string
+}
+
+func (pcc *CatCommand) Execute(handler Handler) {
+	res := pcc.Arg1 + pcc.Arg2
+	handler.Post(&PrintCommand{Arg: res})
+}
