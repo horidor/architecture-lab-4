81c1a6c12e20c8b25d29432f9b2a4f2800aa9003
gapiyka <50524296+gapiyka@users.noreply.github.com>
2022-06-11 14:58:46 +0300

    feat: add command-queue implementation

--- engine/engine.go
+++ engine/engine.go
@@ -18,3 +18,36 @@ type Queue struct {
 	signalToAwait chan struct{}
 	awaiting      bool
 }
+
+func (q *Queue) push(command Command) {
+	q.Lock()
+	defer q.Unlock()
+
+	q.cmdArray = append(q.cmdArray, command)
+	if q.awaiting {
+		q.awaiting = false
+		q.signalToAwait <- struct{}{}
+	}
+
+}
+
+func (q *Queue) pull() Command {
+	q.Lock()
+	defer q.Unlock()
+
+	if q.empty() {
+		q.awaiting = true
+		q.Unlock()
+		<-q.signalToAwait
+		q.Lock()
+	}
+
+	res := q.cmdArray[0]
+	q.cmdArray[0] = nil
+	q.cmdArray = q.cmdArray[1:]
+	return res
+}
+
+func (q *Queue) empty() bool {
+	return len(q.cmdArray) == 0
+}
